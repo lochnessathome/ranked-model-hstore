4d7cf15d9ea7b932b73728a6b1440b7abfaf73d9
Dmitriy Komaritskiy <lochnessathome@gmail.com>
2015-08-26 15:18:28 +0300

    Run tests

--- test/support/active_record.rb
+++ test/support/active_record.rb
@@ -4,14 +4,14 @@ require 'logger'
 root_path = File.expand_path(File.dirname(__FILE__) + '../../../')
 
 log_file = File.join(root_path, 'log/database.log')
-db_config = File.join(root_path, 'spec/support/database.yml')
+db_config = File.join(root_path, 'test/support/database.yml')
 
 ActiveRecord::Base.logger = Logger.new(log_file)
 ActiveRecord::Base.configurations = YAML::load(IO.read(db_config))
-ActiveRecord::Base.establish_connection
+ActiveRecord::Base.establish_connection('test')
 
 ActiveRecord::Schema.define :version => 0 do
-  create_table :entities, :force => true do |t|
+  create_table :entities, force: true do |t|
     t.string :name
   end
 end
